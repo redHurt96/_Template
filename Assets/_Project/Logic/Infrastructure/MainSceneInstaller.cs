df04c8024295d606c33a41ff7639a5a31d024001
redHurt <62645756+redHurt96@users.noreply.github.com>
2023-11-28 23:20:46 +0300

    Import packages & setup zenject

---
+++ Assets/_Project/Logic/Infrastructure/MainSceneInstaller.cs
@@ -0,0 +1,12 @@
+using Zenject;
+
+namespace _Project.Logic.Infrastructure
+{
+    public class MainSceneInstaller : MonoInstaller
+    {
+        public override void InstallBindings()
+        {
+            base.InstallBindings();
+        }
+    }
+}
---
+++ Assets/_Project/Logic/Infrastructure/ProjectInstaller.cs
@@ -0,0 +1,12 @@
+using Zenject;
+
+namespace _Project.Logic.Infrastructure
+{
+    public class ProjectInstaller : MonoInstaller
+    {
+        public override void InstallBindings()
+        {
+            base.InstallBindings();
+        }
+    }
+}
